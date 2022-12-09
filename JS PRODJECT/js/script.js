36cb1cf17c68de9cdb77f59797dd2010ae8c4e90
ProgibatorEggs <111351345+ProgibatorEggs@users.noreply.github.com>
2022-12-09 18:05:44 +0300

    123
    c

--- JS PRODJECT/js/script.js
+++ JS PRODJECT/js/script.js
@@ -1,3 +1,26 @@
 "use strict"
+function searchAnnagram()
+{
+    
+}
 
-alert("Gg");
+function aclean(arr)
+{
+    let letter, annagram;
+    for(let i = 0; i<arr.length;i++)
+    {
+        arr[i].toLowerCase();
+        for(let j = i + 1; j > i;i++)
+        {
+            if(arr[i].length == arr[j].length)
+            {
+                
+            }
+        }
+    }
+    alert(arr);
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+alert( aclean(arr) ); 
