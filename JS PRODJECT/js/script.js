53c5f07faf45c439dc8c6f1dd8c058581a6d4e05
ProgibatorEggs <111351345+ProgibatorEggs@users.noreply.github.com>
2022-12-10 21:18:48 +0300

    Update script.js

--- JS PRODJECT/js/script.js
+++ JS PRODJECT/js/script.js
@@ -1,21 +1,30 @@
 "use strict"
-function searchAnnagram()
+function searchAnnagram(letter, annagram)
 {
-    
+    let coinsidence;
+    if(letter.length != annagram.length) return false;
+    else
+    {
+        for(let i = 0; i<arr.length;i++)
+        {
+            for(let j = 0; j < annagram.length;i++)
+            { 
+               if(letter.substr(i,1) == annagram.substr(j,1)) coinsidence++;
+            }
+        }
+        if(coinsidence == letter.length) return true;
+    }
 }
 
 function aclean(arr)
 {
-    let letter, annagram;
+    let letter, annagram,clearedArray;
     for(let i = 0; i<arr.length;i++)
     {
         arr[i].toLowerCase();
         for(let j = i + 1; j > i;i++)
         {
-            if(arr[i].length == arr[j].length)
-            {
-                
-            }
+            if(searchAnnagram(arr[i],arr[j])) arr.splice(j,1); 
         }
     }
     alert(arr);
